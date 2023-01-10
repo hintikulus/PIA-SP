12745ac475d466bc7a60e900f336b840dd0d0097
Jan Hinterholzinger <hintik@icloud.com>
2023-01-10 15:20:42 +0100

    Podpora vícejazyčnosti

--- app/modules/Base/BasePresenter.php
+++ app/modules/Base/BasePresenter.php
@@ -7,7 +7,9 @@ use App\Model\Security\SecurityUser;
 use App\UI\Control\TFlashMessage;
 use App\UI\Control\TModuleUtils;
 use Contributte\Application\UI\Presenter\StructuredTemplates;
+use Contributte\Translation\LocalesResolvers\Session;
 use Nette\Application\UI\Presenter;
+use Nette\Localization\ITranslator;
 
 /**
  * @property-read TemplateProperty $template
@@ -16,8 +18,20 @@ use Nette\Application\UI\Presenter;
 abstract class BasePresenter extends Presenter
 {
 
+	/** @var ITranslator @inject */
+	public $translator;
+
+	/** @var Session @inject */
+	public $translatorSessionResolver;
+
 	use StructuredTemplates;
 	use TFlashMessage;
 	use TModuleUtils;
+
+	public function handleChangeLocale(string $locale): void
+	{
+		$this->translatorSessionResolver->setLocale($locale);
+		$this->redirect('this');
+	}
 
 }
